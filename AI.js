918cf0e57f0bdd7ccfb9e83c844e95365451c040
yada <makise_kurisu@icloud.com>
2016-03-17 00:23:40 +0800

    GUI for fivechess

---
+++ AI.js
@@ -0,0 +1,17 @@
+function AIPlay (chessData, bd) {
+  if (bd) {
+    return AIPlaybd(chessData);
+  }
+  else {
+    return AIPlaynbd(chessData);
+  }
+}
+
+//无禁手
+function AIPlaynbd (chessData) {
+
+}
+
+function AIPlaybd (chessData) {
+  return AIPlaynbd(chessData);
+}
---
+++ fivechess.js
@@ -0,0 +1,172 @@
+var WHITE = -1, BLACK = 1, NOPUT = 0;
+var canvas, context;
+var winner = NOPUT;
+var nowColor = BLACK;
+var blackDifficult = false;
+var img_w = new Image(), img_b = new Image();
+var histMove = Array();
+img_w.src = "images/white.png";
+img_b.src = "images/black.png";
+
+var chessData = new Array(15);
+for (var i = 0; i < chessData.length; ++i) {
+  chessData[i] = new Array(15);
+  for (var j = 0; j < chessData[i].length; ++j) {
+    chessData[i][j] = NOPUT;
+  }
+}
+
+function drawRect () {
+  canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d");
+  for (var i = 20; i < 600; i += 40) {
+    context.beginPath();
+    context.moveTo(20, i);
+    context.lineTo(580, i);
+    context.closePath();
+    context.stroke();
+    context.beginPath();
+    context.moveTo(i, 20);
+    context.lineTo(i, 580);
+    context.closePath();
+    context.stroke();
+  }
+}
+
+function isValid (x, y) {
+  return x>=0&&x<chessData[0].length&&y>=0&&y<chessData.length;
+}
+
+function countLine (x, y, dx, dy) {
+  if( !isValid(x, y) ) {
+    return 0;
+  }
+  var chess = chessData[x][y];
+
+  var ax = x, ay = y;
+  var bx = x, by = y;
+  while (isValid(ax, ay) && chess==chessData[ax][ay]) {
+    ax += dx;
+    ay += dy;
+  }
+  while (isValid(bx, by) && chess==chessData[bx][by]) {
+    bx -= dx;
+    by -= dy;
+  }
+  var length = Math.max(Math.abs(ax-bx), Math.abs(ay-by)) - 1;
+
+  return length;
+}
+
+function isOver(x, y) {
+  if ( !isValid(x, y) ) {
+    return NOPUT;
+  }
+  var chess = chessData[x][y];
+  var direction = Array(8);
+  direction[0]=0;direction[1]=1;
+  direction[2]=1;direction[3]=0;
+  direction[4]=1;direction[5]=1;
+  direction[6]=1;direction[7]=-1;
+
+  var nThree = 0, nFour = 0, nFive = 0, nSix = 0;
+  for (var i = 0; i < direction.length; i+=2) {
+    var tmp = countLine(x, y, direction[i], direction[i+1]);
+    if (tmp==3) nThree++;
+    else if(tmp==4) nFour++;
+    else if(tmp==5) nFive++;
+    else if(tmp>5) nSix++;
+  }
+
+  if (blackDifficult && chess == BLACK) {
+    if (nFive) {
+      return chess;
+    }
+    if (nSix || nThree >= 2 || nFour >= 2)
+      return WHITE;
+  }
+  else {
+    if (nFive || nSix)
+      return chess;
+  }
+  return NOPUT;
+}
+
+function drawChess(chess, x, y) {
+  if (!isValid(x, y)) {
+    return;
+  }
+  chessData[x][y] = chess;
+  if (chess == WHITE) {
+    context.drawImage(img_w, x*40+5, y*40+5);
+  }
+  else {
+    context.drawImage(img_b, x*40+5, y*40+5);
+  }
+  winner = isOver(x, y);
+  if (winner) {
+    if (winner == WHITE) {
+      alert("WHITE wins");
+    }
+    else {
+      alert("BLACK wins");
+    }
+  }
+}
+
+function posit(x, y) {
+    histMove.push(x);
+    histMove.push(y);
+    drawChess(nowColor, x, y);
+    nowColor = -nowColor;
+}
+
+function play(e) {
+  if (winner) {
+    alert("Game is over");
+    return;
+  }
+  var x = parseInt(e.offsetX/40);
+  var y = parseInt(e.offsetY/40);
+  if (!isValid(x, y)) {
+    return;
+  }
+  if (chessData[x][y] != NOPUT) {
+    alert("You can't play here");
+    return ;
+  }
+  posit(x, y);
+}
+
+function unplay() {
+  if (histMove.length == 0) {
+    alert("No hist");
+    return;
+  }
+  var y = histMove.pop();
+  var x = histMove.pop();
+  console.log(x + "," + y);
+
+  context.fillStyle="white";
+  context.fillRect(x*40, y*40, 40, 40);
+  context.beginPath();
+  context.moveTo(x*40, y*40+20);
+  context.lineTo(x*40+40, y*40+20);
+  context.closePath();
+  context.stroke();
+  context.beginPath();
+  context.moveTo(x*40+20, y*40);
+  context.lineTo(x*40+20, y*40+40);
+  context.closePath();
+  context.stroke();
+
+  chessData[x][y] = NOPUT;
+  nowColor = -nowColor;
+  winner = NOPUT;
+}
+
+function init() {
+  while (histMove.length > 0) {
+    unplay();
+  }
+}
